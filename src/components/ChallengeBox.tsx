544b952c576b31c290abf5d522a2f60d64081575
olliveer <j.oliveira90@live.com>
2021-02-24 14:38:46 -0300

    challenge contexts

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,28 +1,30 @@
 
 
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallangeBox() {
-   const hasActiveChallenge = true;
+   const { activeChallenge } = useContext(ChallengesContext);
 
    return (
       <div className={styles.challengeBoxContainer}>
-         {hasActiveChallenge
+         {activeChallenge
             ? (
                <div className={styles.challengeActive}>
-                  <header>Ganhe 400 xp</header>
+                  <header>Ganhe {activeChallenge.amount} xp</header>
 
                   <main>
-                     <img src="icons/body.svg" alt="Body" />
+                     <img src={`icons/${activeChallenge.type}.svg`} alt="Type" />
                      <strong>Novo desafio</strong>
-                     <p>Levante e faça uma caminhada</p>
+                     <p>{activeChallenge.description}</p>
                   </main>
 
                   <footer>
                      <button
                         type="button"
                         className={styles.challengeFailedButton}
-                        // onClick={}
+                     // onClick={}
                      >
                         Falhei
                         </button>
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 import { AiFillCheckCircle } from "react-icons/ai";
+import { ChallengesContext } from '../contexts/ChallengesContext';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+    const { startNewChallenge } = useContext(ChallengesContext);
+
     const [time, setTime] = useState(0.05 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFineshed] = useState(false);
@@ -34,6 +37,7 @@ export function Countdown() {
         } else if (isActive && time === 0) {
             setHasFineshed(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
